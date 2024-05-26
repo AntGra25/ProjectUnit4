402e704982e8b3c225fe97ccace2d93fc300afb2
Antoni Grabka <antoni.grabka@gmail.com>
2024-05-27 08:39:43 +0900

    added tools.py file with DatabaseWorker class

---
+++ tools.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+class DatabaseWorker:
+    def __init__(self, name:str):
+        self.name_db = name
+        # Step 1: create a connection to the file
+        self.connection = sqlite3.connect(self.name_db)
+        self.cursor = self.connection.cursor()
+
+    def run_query(self, query:str):
+        self.cursor.execute(query)  # run query
+        self.connection.commit()  # save changes
+
+    def insert(self, query:str):
+        self.run_query(query)
+
+    def search(self, query:str, multiple=False):
+        results = self.cursor.execute(query)
+        if multiple:
+            return results.fetchall()  # return multiple rows
+        return results.fetchone()  # return a single value
+
+    def create(self, table_name: str):
+        query = f"""CREATE TABLE if not exists {table_name}(
+                id INTEGER PRIMARY KEY,
+                email text NOT NULL unique,
+                password VARCHAR(256),
+                username text NOT NULL)"""
+        self.run_query(query)
+
+    def close(self):
+        self.connection.close()
