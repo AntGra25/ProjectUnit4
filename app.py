67fbd9e8c3daedc61d1b0da41057c7fb6670dc00
Antoni Grabka <antoni.grabka@gmail.com>
2024-05-29 09:38:23 +0900

    created login system and users table

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from tools import DatabaseWorker
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    if request.method == 'POST':
+        uname = request.form.get('uname')
+        password = request.form.get('psw')
+        db = DatabaseWorker('Reddit.db')
+        user = db.search(f"SELECT * FROM users WHERE username='{uname}'")
+        if user:
+            if user[2] == password:
+                response = make_response(redirect(url_for('get_all_posts')))
+                response.set_cookie('user_id', str(user[0]))
+                db.close()
+                return response
 
 
 @app.route('/register')
